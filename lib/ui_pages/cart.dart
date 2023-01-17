4ec5425f59eec0f6b7ec1a5b345815b9a87a1832
Kamran Mehar <kamranmehar005@gmail.com>
2023-01-17 22:28:33 +0500

    completed cart functionality fix bugs in cart

--- lib/ui_pages/cart.dart
+++ lib/ui_pages/cart.dart
@@ -24,12 +24,13 @@ class Cart extends StatefulWidget {
 class _CartState extends State<Cart> {
    StreamController<int> totalStreamController = StreamController<int>.broadcast();
    StreamController<int> itemTotalStreamController = StreamController<int>.broadcast();
+   StreamController<DatabaseEvent> cartDataStreamController = StreamController<DatabaseEvent>.broadcast();
     int totalPrice=0;
 
   @override
   Widget build(BuildContext context) {
     var ref=FirebaseDatabase.instance
-        .ref("Users/"+FirebaseAuth.instance.currentUser!.uid+"/Cart").onValue.asBroadcastStream();
+        .ref("Users/${FirebaseAuth.instance.currentUser!.uid}/Cart").onValue.asBroadcastStream();
     return Hero(
       tag: 'cart',
       child: Container(
@@ -58,6 +59,176 @@ class _CartState extends State<Cart> {
                 Expanded(
                     child:
                   StreamBuilder(
+                stream: cartDataStreamController.stream,
+                builder: (context,/*AsyncSnapshot<DatabaseEvent>*/ snapshot) {
+
+                    if (snapshot.data != null) {
+                      Map<dynamic, dynamic> ?map = snapshot.data!.snapshot.value as dynamic;
+                       List<dynamic> list = [];
+                       list.clear();
+                       if(map!=null){
+                       list=map.values.toList();
+                        totalPrice=0;
+                          for(int i=0;i<list.length;i++){
+                            int quantity=list[i]['quantity'];
+                            int price=int.parse(list[i]['price']);
+                            totalPrice=totalPrice+(quantity*price);
+                          }
+                          addTotalToStream(totalPrice);
+                       addTotalItemsToStream(list.length);
+
+                        return ListView.builder(
+                            physics:const BouncingScrollPhysics(),
+                            itemCount: list.length,
+                            itemBuilder: (context, index) {
+                              return CartItemTile(
+                                  name: list[index]['name'],
+                                  price: list[index]['price'],
+                                  titleImage: list[index]['titleImage'],
+                                  coverImage: list[index]['coverImage'],
+                                  quantity: list[index]['quantity'],
+                                  onCancelClick: () async {
+                                    ///Delete from cart list
+                                    DatabaseReference ref = FirebaseDatabase
+                                        .instance
+                                        .ref("Users/${FirebaseAuth.instance.currentUser!.uid}/Cart/" + list[index]['name']);
+                                    ref.remove().then((value) {
+                                      Util_.showToast(list[index]['name'] +
+                                          " Removed from cart");
+                                      Home.updateCartBadge();
+                                    });
+                                  },
+                                  incrementPriceCallBack: (int v) async{
+
+                                    FirebaseDatabase.instance
+                                        .ref("Users/${FirebaseAuth.instance.currentUser!.uid}/Cart/"+list[index]['name']).update({
+                                      'quantity': v,
+                                    }).onError((error, stackTrace) => print(error.toString()));
+                                    getCartData();
+
+                                  }
+                                  ,
+                                  decrementPriceCallback: (int v) {
+
+                                    FirebaseDatabase.instance
+                                        .ref("Users/${FirebaseAuth.instance.currentUser!.uid}/Cart/"+list[index]['name']).update({
+                                      'quantity': v,
+                                    }).onError((error, stackTrace) => print(error.toString()));
+                                     getCartData();
+                                  });
+                            });
+                      } else {
+                         totalPrice=0;
+                         addTotalToStream(0);
+                         addTotalItemsToStream(0);
+                        return const Center(child: Text("NO RECORD"),);
+                      }
+                    } else {
+                      return  Center(child: TextButton(
+                          onPressed: (){
+                            setState(() {});
+                          }, child:const CircularProgressIndicator(color: Colors.black,)),);
+                    }
+
+                }
+                ),
+                ),
+                Container(
+                  width: double.infinity,
+                  height: 100,
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.white, width: 1),
+                      color: Colors.white.withOpacity(0.3),
+                      borderRadius: const BorderRadius.only(
+                          topLeft: Radius.circular(20), topRight: Radius
+                          .circular(20))
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      ///Price and Item Count
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          StreamBuilder<int>(
+                              stream: totalStreamController.stream.asBroadcastStream(),
+                              builder: (context,snap){
+                                return HeadingText(text: "Total Amount: ${snap.data!=null?snap.data.toString():0}",
+                                  color: Colors.white,
+                                  size: 20,);
+                              }),
+                          StreamBuilder<int>(
+                              stream: itemTotalStreamController.stream.asBroadcastStream(),
+                              builder: (context,snap){
+                                return HeadingText(text: "Total Items: ${snap.data!=null?snap.data.toString():0}",
+                                  color: Colors.white,
+                                  size: 18,);
+                              }),
+                        ],
+                      ),
+
+                      ///Buy button
+                      Container(
+                        padding: const EdgeInsets.all(5),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15),
+                            color: Colors.pink
+                        ),
+                        child: TextButton(onPressed: () {
+                          if(totalPrice!=0){
+                            Navigator.push(context, MaterialPageRoute(builder: (context)=> OrderConfirmationScreen()));
+                          }else{
+                            Util_.showToast("Cart is Empty !");
+                          }
+                        },
+                            child: HeadingText(text: "Buy",
+                              color: Colors.white,
+                              size: 20,)),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+        ),
+      ),
+    );
+  }
+
+addTotalToStream(int num)async{
+    totalStreamController.sink.add(num);
+}
+addTotalItemsToStream(int num)async{
+    itemTotalStreamController.sink.add(num);
+}
+
+  getCartData()async{
+
+   var ref=FirebaseDatabase.instance
+      .ref("Users/${FirebaseAuth.instance.currentUser!.uid}/Cart");
+    DatabaseEvent event=await ref.once();
+   cartDataStreamController.sink.add(event);
+}
+
+@override
+  void initState() {
+    Timer.periodic(const Duration(seconds: 3), (timer) {
+      getCartData();
+    });
+    // TODO: implement initState
+    super.initState();
+  }
+
+@override
+  void dispose() {
+    Home.updateCartBadge();
+    super.dispose();
+  }
+
+}
+
+/*StreamBuilder(
                 stream: ref,
                 builder: (context,AsyncSnapshot<DatabaseEvent> snapshot) {
 
@@ -125,6 +296,7 @@ class _CartState extends State<Cart> {
                         return const Center(child: Text("NO RECORD"),);
                       }
                     } else {
+                      //setState(() {});
                       return  Center(child: TextButton(
                           onPressed: (){
                             setState(() {});
@@ -133,78 +305,4 @@ class _CartState extends State<Cart> {
                     }
 
                 }
-                ),
-                ),
-                Container(
-                  width: double.infinity,
-                  height: 100,
-                  decoration: BoxDecoration(
-                      border: Border.all(color: Colors.white, width: 1),
-                      color: Colors.white.withOpacity(0.3),
-                      borderRadius: const BorderRadius.only(
-                          topLeft: Radius.circular(20), topRight: Radius
-                          .circular(20))
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      ///Price and Item Count
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          StreamBuilder<int>(
-                              stream: totalStreamController.stream.asBroadcastStream(),
-                              builder: (context,snap){
-                                return HeadingText(text: "Total Amount: ${snap.data!=null?snap.data.toString():0}",
-                                  color: Colors.white,
-                                  size: 20,);
-                              }),
-                          StreamBuilder<int>(
-                              stream: itemTotalStreamController.stream.asBroadcastStream(),
-                              builder: (context,snap){
-                                return HeadingText(text: "Total Items: ${snap.data!=null?snap.data.toString():0}",
-                                  color: Colors.white,
-                                  size: 18,);
-                              }),
-                        ],
-                      ),
-
-                      ///Buy button
-                      Container(
-                        padding: const EdgeInsets.all(5),
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(15),
-                            color: Colors.pink
-                        ),
-                        child: TextButton(onPressed: () {
-                              Navigator.push(context, MaterialPageRoute(builder: (context)=> OrderConfirmationScreen()));
-                        },
-                            child: HeadingText(text: "Buy",
-                              color: Colors.white,
-                              size: 20,)),
-                      )
-                    ],
-                  ),
-                )
-              ],
-            ),
-        ),
-      ),
-    );
-  }
-
-addTotalToStream(int num)async{
-    totalStreamController.sink.add(num);
-}
-addTotalItemsToStream(int num)async{
-    itemTotalStreamController.sink.add(num);
-}
-
-@override
-  void dispose() {
-    Home.updateCartBadge();
-    super.dispose();
-  }
-
-}
+                ),*/
--- lib/ui_pages/order_confirmation_page.dart
+++ lib/ui_pages/order_confirmation_page.dart
@@ -1,4 +1,10 @@
+import 'dart:async';
+
+import 'package:donut_hub/util/constents.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+
 
 class OrderConfirmationScreen extends StatefulWidget {
    OrderConfirmationScreen({Key? key}) : super(key: key);
@@ -9,27 +15,64 @@ class OrderConfirmationScreen extends StatefulWidget {
 
 class _OrderConfirmationScreenState extends State<OrderConfirmationScreen> {
 
+TextEditingController addressController=TextEditingController();
 
   @override
   Widget build(BuildContext context) {
+    final _formKey = GlobalKey<FormState>();
     return Container(
-      decoration: const BoxDecoration(
-        gradient: RadialGradient(colors: [
+      decoration:  const BoxDecoration(
+        image: DecorationImage(image: AssetImage('lib/images/donuts/purple_donut.gif'),fit: BoxFit.fitHeight,
+            opacity: 1),
+      /*  gradient: RadialGradient(colors: [
           Color(0xff7ba7c2),
           Color(0xff517396),
-        ],
-            radius: 1
-        ),
+        ], radius: 1
+        ),*/
       ),
       child: Scaffold(
-        extendBodyBehindAppBar: true,
+        appBar: AppBar(
+          centerTitle: true,
+          iconTheme: const IconThemeData(color: Colors.white),
+          title: HeadingText(text: "Confirmation", color: Colors.white),
+          automaticallyImplyLeading: true,
+          elevation: 0,
+          backgroundColor: Colors.transparent,
+        ),
+        extendBodyBehindAppBar: false,
         backgroundColor: Colors.transparent,
         body:
         Column(
           children: [
-            Form(child: TextFormField(
+            NormalText(text: "Enter Your Order Delivery Address", color: Colors.white,size: 18,),
+            Form(
+                key: _formKey,
+                child:
+                Container(
+                  padding:const EdgeInsets.symmetric(horizontal: 5),
+                  margin: const EdgeInsets.all( 12),
+                  decoration: BoxDecoration(
 
-            )),
+                      borderRadius: BorderRadius.circular(15),
+                      color: Colors.grey[400],
+                      border: Border.all(color: Colors.white,width: 1)
+                  ),
+                  child:   TextFormField(
+                    validator: (value){
+                      if(value!.isEmpty){
+                        return "Address is Empty";
+                      }
+                    },
+                    controller: addressController,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: const InputDecoration(
+                        suffixIcon: Icon(Icons.location_on,color: Colors.black26,),
+                        border: InputBorder.none,
+                        hintText: 'Address',
+                        hintStyle: TextStyle(color: Colors.black26)
+                    ),
+                  ),
+                ),),
           ],
         ),
       ),
